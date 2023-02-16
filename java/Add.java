1eed85bc326d8a573ce7309f8a9af4a589fd6b21
SCI02067 <minami.naho@nifty.co.jp>
2023-02-16 18:50:56 +0900

    javaのGitHub Actionsのテスト追加

---
+++ java/Add.java
@@ -0,0 +1,5 @@
+public class Add {
+    public static int SampleAdd(int x, int y) {
+        return x + y;
+    }
+}
---
+++ java/AddTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AddTest {
+    @Test
+    public void SampleAddTest() {
+        assertEquals(8, Add.SampleAdd(3, 5));
+    }
+}
